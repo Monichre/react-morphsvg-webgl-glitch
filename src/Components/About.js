c3085f6e519f8e2f566ede1a0b342fb618bc256c
Liam Ellis <liamhellis@gmail.com>
2017-10-16 22:02:31 -0400

    bugs

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,12 +1,188 @@
 import React, {Component} from 'react'
+import anime from 'animejs'
 import '../css/About.css'
 
+// import TabsNav from '../utils/TabsNav'
+
+ 
+
+
 export default class About extends Component {
+
+    // openTabCallback(anim, idx, tab) {
+
+
+    //     const contentItems = [].slice.call(document.querySelectorAll('.tabscontent > .tabscontent__item'))
+    //     const backCtrl = document.querySelector('.tabscontent > button.btn--back')
+    //     const menuCtrl = document.querySelector('button.btn--menu')
+    //     let isContentShown = false
+    //     let current
+
+
+    //     if( anim.progress > 60 && !isContentShown ) {
+    //         isContentShown = true;
+    //         current = idx;
+
+    //         var contentItem = contentItems[idx],
+    //             content = {
+    //                 img: contentItem.querySelector('img.poster__img'),
+    //                 title: contentItem.querySelector('.poster__title'),
+    //                 deco: contentItem.querySelector('.poster__deco'),
+    //                 box: contentItem.querySelector('.poster__box'),
+    //                 number: contentItem.querySelector('.poster__number')
+    //             };
+
+    //         // Hide the content elements.
+    //         content.img.style.opacity = content.title.style.opacity = content.deco.style.opacity = content.box.style.opacity = content.number.style.opacity = 0;
+    //         // Show content item.
+    //         contentItem.style.opacity = 1;
+    //         contentItem.classList.add('tabscontent__item--current');
+
+    //         // Animate content elements in.
+    //         anime.remove([content.img, content.title, content.box, content.number, content.deco]);
+    //         anime({
+    //             targets: [content.img, content.title, content.box, content.number, content.deco],
+    //             easing: 'easeOutExpo',
+    //             duration: function(t,i) {
+    //                 return 600 + i*100;
+    //             },
+    //             scaleX: function(t,i) {
+    //                 return i === 0 ? [0,1] : 1;
+    //             },
+    //             translateX: function(t,i) {
+    //                 return [-80-i*150,0];
+    //             },
+    //             rotate: function(t,i) {
+    //                 return i === 2 ? [-40,0] : 0;
+    //             },
+    //             opacity: {
+    //                 value: 1,
+    //                 duration: 300,
+    //                 easing: 'linear'
+    //             }
+    //         });
+    //     }
+    // }
+    
+    
+    // closeTabs(tnav, contentItems, current, isContentShown, backCtrl, menuCtrl) {
+    //     if( !tnav.isOpen ) return;
+
+    //     const contentItem = contentItems[current],
+    //         content = {
+    //             img: contentItem.querySelector('img.poster__img'),
+    //             title: contentItem.querySelector('.poster__title'),
+    //             deco: contentItem.querySelector('.poster__deco'),
+    //             box: contentItem.querySelector('.poster__box'),
+    //             number: contentItem.querySelector('.poster__number')
+    //         };
+
+    //     // Hide the content elements.
+    //     anime.remove([content.img, content.title, content.box, content.number, content.deco]);
+    //     // Animate content elements out.
+    //     anime({
+    //         targets: [content.deco, content.number, content.box, content.title, content.img],
+    //         easing: 'easeInOutCubic',
+    //         duration: function(t,i) {
+    //             return 600 + i*100;
+    //         },
+    //         delay: function(t,i,c) {
+    //             return (c-i-1)*35;
+    //         },
+    //         translateX: function(t,i) {
+    //             return [0,-200-i*150];
+    //         },
+    //         rotate: function(t,i) {
+    //             return i === 2 ? [0,-40] : 0;
+    //         },
+    //         opacity: {
+    //             value: 0,
+    //             duration: 450
+    //         },
+    //         update: function(anim) {
+    //             if( anim.progress > 20 && isContentShown ) {
+    //                 isContentShown = false;
+    //                 // Close tab.
+    //                 tnav.close();
+    //             }
+    //         },
+    //         complete: function() {
+    //             // Hide content item.
+    //             contentItem.style.opacity = 0;
+    //             contentItem.classList.remove('tabscontent__item--current');
+    //         }
+    //     });
+
+    //     // Hide back ctrl
+    //     anime.remove(backCtrl);
+    //     anime({
+    //         targets: backCtrl,
+    //         duration: 800,
+    //         easing: 'easeOutExpo',
+    //         scale: [1,0],
+    //         opacity: {
+    //             value: 0,
+    //             duration: 200,
+    //             easing: 'linear'
+    //         }
+    //     });
+    // }
+    // toggleTabs(tnav, menuCtrl) {
+        
+    //     var state = tnav.toggleVisibility();
+    //     if( state === 0 ) {
+    //         menuCtrl.classList.remove('btn--menu-active');
+    //     }
+    //     else if( state === 1 ) {
+    //         menuCtrl.classList.add('btn--menu-active');
+    //     }
+    // }
+
+    // componentDidMount(){
+        
+    //     const contentItems = [].slice.call(document.querySelectorAll('.tabscontent > .tabscontent__item'))
+    //     const backCtrl = document.querySelector('.tabscontent > button.btn--back')
+    //     const menuCtrl = document.querySelector('button.btn--menu')
+    //     const isContentShown = false
+    //     let current
+    //     const nav = document.querySelector('.tabsnav')
+
+    //     const tnav = new TabsNav(nav, {
+    //             movable: 'all',
+    //             extramovable: '.content',
+    //             layout: 'vertical',
+    //             animeduration: 700,
+    //             animeeasing: 'easeInOutCubic',
+    //             animedelay: 100,
+    //             onOpenBarsUpdate: this.openTabCallback,
+    //             onOpenTab: function() {
+    //                 // Show the back button after the tab is open.
+    //                 anime({
+    //                     targets: backCtrl,
+    //                     duration: 800,
+    //                     easing: 'easeOutExpo',
+    //                     scale: [0,1],
+    //                     opacity: {
+    //                         value: 1,
+    //                         duration: 300,
+    //                         easing: 'linear'
+    //                     }
+    //                 });
+    //             }
+    //         })
+    //         console.log(tnav)
+    
+    //     backCtrl.addEventListener('click', this.closeTabs(tnav, contentItems, current, isContentShown, backCtrl, menuCtrl))
+    //     menuCtrl.addEventListener('click', this.toggleTabs(tnav, menuCtrl))
+    // }
+    
+           
+    
 
     render() {
         
                 return (
-                    <div className="About">
+                    <div id="About" className="About demo-2">
  
                         
                             <main className="view">
